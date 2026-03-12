a8de18187dce09a001ff9c4bb4b785e290d7e8f0
tjsdnd489856 <tjsdnd489856@gmail.com>
2026-03-12 06:03:30 +0000

    feat: 앱 전역에 렌즈 프로바이더 연결

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/lens_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => LensProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
