b489aaeb69d111084bd6070be0a468bcfc8cc289
tjsdnd489856-design <tjsdnd489856@github.com>
2026-03-13 16:52:53 +0900

    chore: 구글 맵 API 키 설정 및 네이티브 인프라 가동 준비 완료

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,7 +9,7 @@ import GoogleMaps // 구글 맵 라이브러리 추가
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     // [신규] iOS 구글 맵 API 키 초기화
-    GMSServices.provideAPIKey("YOUR_IOS_API_KEY")
+    GMSServices.provideAPIKey("AIzaSyBcXgANLU-mnTOx0BWZK6mIH-pyaf0Ptgs")
     
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
